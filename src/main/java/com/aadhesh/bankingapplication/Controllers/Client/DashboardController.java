5b65b490c91f7ddfe78c8de27d1c4f71903fc748
aadheshofficial <aadheshoffcial@gmail.com>
2024-10-14 09:54:47 +0530

    created client dashboard front end

---
+++ src/main/java/com/aadhesh/bankingapplication/Controllers/Client/DashboardController.java
@@ -0,0 +1,20 @@
+package com.aadhesh.bankingapplication.Controllers.Client;
+
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
+public class DashboardController {
+    public Text user_name;
+    public Label login_date;
+    public Label checking_balance;
+    public Label checking_acc_number;
+    public Label savings_balance;
+    public Label savings_acc_number;
+    public Label income_amt;
+    public Label expense_amt;
+    public ListView transaction_listview;
+    public TextField payee_filed;
+    public TextField amount_file;
+    public TextArea message_filed;
+    public Button send_button;
+}
